4465f2eb36ecb97d002e9d337397ba96c76806ab
Mahaut <mmahaut@ensc.fr>
2020-09-08 10:35:56 +0200

    apply on all subjects

--- 01_parcellisation.py
+++ 01_parcellisation.py
@@ -1,12 +1,14 @@
 from nilearn.regions import Parcellations
 import time
 import os
+import json
+
 
 def parcellation(sub_name="NYU_0051091"):
     start = time.time()
     rena = Parcellations(
         method="rena",
-        n_parcels=5000,
+        n_parcels=200,
         standardize=False,
         smoothing_fwhm=2.0,
         scaling=True,
@@ -19,13 +21,16 @@ def parcellation(sub_name="NYU_0051091"):
     )
     print("ReNA 5000 clusters: %.2fs" % (time.time() - start))
     rena_labels_img = rena.labels_img_
-
     # Now, rena_labels_img are Nifti1Image object, it can be saved to file
     # with the following code:
     out_dir = "/scratch/mmahaut/data/abide/graph_classification/{}".format(sub_name)
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
-    rena_labels_img.to_filename(os.path.join(out_dir,'rena_parcellation.nii.gz')
+    rena_labels_img.to_filename(os.path.join(out_dir, "rena_parcellation.nii.gz"))
 
-# visualisation and saving : https://nilearn.github.io/auto_examples/03_connectivity/plot_data_driven_parcellations.html#sphx-glr-auto-examples-03-connectivity-plot-data-driven-parcellations-py
 
+if __name__ == "__main__":
+    subs_list_file = open("subs_list_asd.json")
+    subs_list = json.load(subs_list_file)
+    for sub_name in subs_list:
+        parcellation(sub_name)
