cca5a2e821f2e75648a9961c42ed9957e58af653
Mahaut <mmahaut@ensc.fr>
2020-09-17 15:43:57 +0200

    path mismatch solved

--- 02_connectivity.py
+++ 02_connectivity.py
@@ -18,6 +18,7 @@ def connectivity(sub_name, out_dir, n_clusters=200):
     ward_connectivity = np.load(
         os.path.join(out_dir, sub_name, "ward_connectivity.npy"), allow_pickle=True
     )
+    print(ward_connectivity.shape)
     for i in range(n_clusters):
         for j in range(i):
             a = ward_connectivity[parcel_submask[i], :][:, parcel_submask[j]]
